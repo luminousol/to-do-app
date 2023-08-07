e91e102f596221e1826dfa5c671f426de28c66d3
luminousol <luminousolding@gmail.com>
2023-08-07 15:35:59 +0900

    git second commit

--- src/main/java/com/luminousol/todoapp/entity/ToDo.java
+++ src/main/java/com/luminousol/todoapp/entity/ToDo.java
@@ -16,13 +16,13 @@ public class ToDo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long toDoId;    // todo 리스트에 부여되는 Id
+    private long id;    // todo 리스트에 부여되는 Id
 
     @Column(nullable = false, length = 255)
-    private String toDoTitle;   // todo 리스트 제목
+    private String title;   // todo 리스트 제목
 
     @Column(nullable = false)
     private int toDoOrder;  // todo 리스트 우선순위
 
-    private boolean toDoCompleted;  // todo 완료 여부
+    private boolean completed;  // todo 완료 여부
 }
